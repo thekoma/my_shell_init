1a540d1eb00434092e44aab3a0615706f0889a14
Andrea Cervesato (TheKoma) <andrea.cervesato@gmail.com>
2022-05-09 22:19:25 +0200

    Update the crontab only if not present to prevent override

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-Usage:
+# Usage:
 ```bash
-cd
 git clone https://github.com/thekoma/my_shell_init --depth=1 ~/.my_shell_init
 bash ~/.my_shell_init/install.sh
 ```
--- install.sh
+++ install.sh
@@ -103,10 +103,12 @@ function install_myself() {
     ln -sf $ME $INIT_SCRIPT
   fi
 
-  CRONTMP=$(mktemp)
-  crontab -l -u $USER |grep -v $SCRIPT_HOME > $CRONTMP
-  echo  "0,15,30,45 * * * * git -C $SCRIPT_HOME pull -q" >> $CRONTMP
-  crontab $CRONTMP
+  if [ "$(crontab -l|grep -c $SCRIPT_HOME)" -lt 1 ]; then
+    CRONTMP=$(mktemp)
+    crontab -l -u $USER |grep -v $SCRIPT_HOME > $CRONTMP
+    echo  -e "# Update shell init git\n0,15,30,45 * * * * git -C $SCRIPT_HOME pull -q" >> $CRONTMP
+    crontab $CRONTMP
+  fi
 }
 
 function main() {
